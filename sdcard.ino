dc79a1139302b8c0dade1c106c36ef20bfdeea8a
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2023-04-25 08:33:55 +0200

    Update sdcard.ino

--- sdcard.ino
+++ sdcard.ino
@@ -12,23 +12,58 @@ Rui Santos
 
 #include <Arduino.h>
 #include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h> // dvarrel 1.1.4
+#include <ESPAsyncWebServer.h> // dvarrel 1.2.6
 #include "FS.h" 
 #include "SD.h"
 #include "SPI.h"
+// pour rtc
+#include <Wire.h>
+#include <DS3231.h> // DS32231 de Andrew Wickert  1.1.2
+// lidar
+#include <SoftwareSerial.h> // !!!!!! version 7.0.0 maxi
+#include "TFMini.h" //Peter Jansen 0.1.0
 
+boolean DEBUG = true;
 String stDeb; 
 String stFin;
 String stList;
 String stHTML;
+
+const char* ssid     = "Base_Desnes";
+const char* password = "";
+
+// Create AsyncWebServer object on port 80 (http) ou 443 (https)
+AsyncWebServer server(80);
+// lidar un seul fil RX + masse et 5v
+SoftwareSerial mySerial(16, 17); //   16: RX, (TX sur 17 inutilise)
+TFMini tfmini;
+
+// rtc
+RTClib rtc;
+
+
+// leds
+int ledpassage = 27;
+int ledpower = 26;
+//
+int dist = 0;
+int dist0 = 0;
+int mini = 0;
+int maxi = 65000;
+int seuil = 20;
+int ecart = 10;
+int precedent = 0;
+int personne = 0;
+int nb = 0;
+///////////////////////////////////////////////////////////
 
 void initVarHtml()
 {
   // debut du fichier index.html
   stDeb += "<!DOCTYPE html>\n";
   stDeb += "<html>\n";
-  stDeb += "<meta charset="+String('"')+"utf-8"+String('"')+"/>\n";
+  stDeb += "<meta charset="+String('"')+"utf-8"+String('"')+">\n";
   stDeb += "  <head>\n";
   stDeb += "    <!-- CSS -> Style -->\n";
   stDeb += "    <style media="+String('"')+"screen"+String('"')+">\n";
@@ -67,7 +102,7 @@ void initVarHtml()
   stDeb += "        the_arr.pop();\n";
   stDeb += "        return the_arr.join("+String('"')+"/"+String('"')+");\n";
   stDeb += "      }\n";
-//
+/*
   stDeb += "      function downloadURI(uri, name){ \n";
   stDeb += "        var link = document.createElement("+String('"')+"a"+'"'+");\n";
   stDeb += "        link.download = name;\n";
@@ -77,12 +112,14 @@ void initVarHtml()
   stDeb += "        document.body.removeChild(link);\n";
   stDeb += "        delete link;\n";
   stDeb += "      }\n";
-//
+*/
   stDeb += "      function download(dataurl, filename) {\n";
+//  stDeb += "alert(dataurl);\n";  //   affiche : "content://medias/external_primary/downloads/20220519"
   stDeb += "        const link = document.createElement("+String('"')+"a"+'"'+");\n";
   stDeb += "        link.href = dataurl;\n";
-//  stDeb += "        link.download = filename;\n";
-  stDeb += "        link.download = "+String("SD/")+"filename;\n";
+//  stDeb += "alert(filename);\n";  //   afiche : "20220519"
+//  stDeb += "alert(link.href);\n";
+  stDeb += "        link.download = filename;\n";
   stDeb += "        link.click();\n";
   stDeb += "      }\n";
 //
@@ -115,13 +152,14 @@ void initVarHtml()
   stDeb += "            console.log("+String('"')+"inputs[i].id: '"+String('"')+" + inputs[i].id +"+String('"')+"'"+String('"')+");\n";
   stDeb += "            concatenateUrlForDownload =\n";
   stDeb += "              removeLastDirectoryPartOf(HOST_URL) +" + String('"')+'/'+String('"') + "+ inputs[i].id;\n";
+//  stDeb += "            concatenateUrlForDownload ="+String('"')+"SD" + String('/') + String('"')+ "+ inputs[i].id;\n";
   stDeb += "            console.log(\n";
   stDeb += "              "+String('"')+"concatenateUrlForDownload: '"+String('"')+" + concatenateUrlForDownload +" + String('"')+"'"+String('"')+"\n";
   stDeb += "            );\n";
             // Download files
             // TODO
-  stDeb += "alert(inputs[i].id);\n";
-  stDeb += "            downloadURI(concatenateUrlForDownload, inputs[i].id);\n";
+  stDeb += "            download(concatenateUrlForDownload, inputs[i].id);\n";
+//  stDeb += "            downloadURI(concatenateUrlForDownload, inputs[i].id);\n";//plante
   stDeb += "          }\n";
 //  stDeb += " deleteFile(SD, "+String('"')+"/index.html"+String('"')+");\n";
   stDeb += "        }\n";
@@ -177,16 +215,13 @@ void initVarHtml()
   Serial.println(stHTML.c_str());
 };
 
+//////////////////
 
-
-
-
-// Functions
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
   String st = "";
   String st0 = "";
   stList = ""; 
-  Serial.printf("Listing directory: %s\n", dirname);
+  //Serial.printf("Listing directory: %s\n", dirname);
 
   File root = fs.open(dirname);
   if(!root){
@@ -204,10 +239,8 @@ void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
       if(levels)
         listDir(fs, file.name(), levels -1);
     } else {
-//        st0 = String(file.name());
-//        if (st0 != "/index.html"){
-        st0 = String(file.name()).substring(1);
-        if (st0 != "index.html"){
+        st0 = String(file.name());
+        if (st0 != String("index.html")){
           st = '"' + st0 + '"'+ "type="+String('"')+"checkbox"+String('"')+" /></td>";
           stList += "<tr><td><input id=" + st;
           stList += "<td>"+st0+"</td></tr>\n";
@@ -215,7 +248,9 @@ void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
     }
     file = root.openNextFile();
   }
-}
+};
+
+//////////////////
 
 void createDir(fs::FS &fs, const char * path){
   Serial.printf("Creating Dir: %s\n", path);
@@ -224,7 +259,9 @@ void createDir(fs::FS &fs, const char * path){
   } else {
     Serial.println("mkdir failed");
   }
-}
+};
+
+//////////////////
 
 void removeDir(fs::FS &fs, const char * path){
   Serial.printf("Removing Dir: %s\n", path);
@@ -233,7 +270,9 @@ void removeDir(fs::FS &fs, const char * path){
   } else {
     Serial.println("rmdir failed");
   }
-}
+};
+
+//////////////////
 
 void readFile(fs::FS &fs, const char * path){
   Serial.printf("Reading file: %s\n", path);
@@ -249,7 +288,9 @@ void readFile(fs::FS &fs, const char * path){
     Serial.write(file.read());
   }
   file.close();
-}
+};
+
+//////////////////
 
 void writeFile(fs::FS &fs, const char * path, const char * message){
   Serial.printf("Writing file: %s\n", path);
@@ -265,7 +306,9 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
     Serial.println("Write failed");
   }
   file.close();
-}
+};
+
+//////////////////
 
 void appendFile(fs::FS &fs, const char * path, const char * message){
   Serial.printf("Appending to file: %s\n", path);
@@ -281,7 +324,9 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
     Serial.println("Append failed");
   }
   file.close();
-}
+};
+
+//////////////////
 
 void renameFile(fs::FS &fs, const char * path1, const char * path2){
   Serial.printf("Renaming file %s to %s\n", path1, path2);
@@ -290,7 +335,9 @@ void renameFile(fs::FS &fs, const char * path1, const char * path2){
   } else {
     Serial.println("Rename failed");
   }
-}
+};
+
+//////////////////
 
 void deleteFile(fs::FS &fs, const char * path){
   Serial.printf("Deleting file: %s\n", path);
@@ -299,7 +346,9 @@ void deleteFile(fs::FS &fs, const char * path){
   } else {
     Serial.println("Delete failed");
   }
-}
+};
+
+//////////////////
 
 void testFileIO(fs::FS &fs, const char * path){
   File file = fs.open(path);
@@ -326,7 +375,6 @@ void testFileIO(fs::FS &fs, const char * path){
     Serial.println("Failed to open file for reading");
   }
 
-
   file = fs.open(path, FILE_WRITE);
   if(!file){
     Serial.println("Failed to open file for writing");
@@ -343,12 +391,7 @@ void testFileIO(fs::FS &fs, const char * path){
   file.close();
 }
 
-// Replace with your network credentials
-const char* ssid = "Realme";
-const char* password = "0952061100";
-
-// Create AsyncWebServer object on port 80 (http) ou 443 (https)
-AsyncWebServer server(80);
+//////////////////
 
 void initSDCard(){
   if(!SD.begin(5)){
@@ -374,41 +417,105 @@ void initSDCard(){
   }
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
   Serial.printf("SD Card Size: %lluMB\n", cardSize);
-}
+};
 
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println(WiFi.localIP());
-}
+///////////////////////////////////////////////////////////
+
+void EcritFichier()
+{
+  DateTime t = rtc.now();
+  String nomfic;
+  String heure ;
+  
+  nomfic = "/" + String(t.year())+ String(t.month()) + String(t.day());
+//  heure = String(t.hour()) + String(t.minute()) + String(t.second()) + char(13) + char(10);
+  heure = String(t.hour()) + String(t.minute()) + String(personne) + char(13) + char(10);
+  Serial.println(heure);
+  if (SD.exists(nomfic.c_str()))
+    appendFile(SD, nomfic.c_str(), heure.c_str());
+  else
+    writeFile(SD, nomfic.c_str(), heure.c_str());
+};
+
+///////////////////////////////////////////////////////////
 
 void setup(){
   Serial.begin(115200);
-  initWiFi();
-  initSDCard();
-  listDir(SD, "/", 0);
-  initVarHtml();
 
+  // led de controle power et passage
+  pinMode(ledpower, OUTPUT);
+  pinMode(ledpassage, OUTPUT);
+
+  // rtc
+  Wire.begin(); 
+  DateTime t = rtc.now();
+  Serial.println(String(t.year())+ "-"+String(t.month()) +"-"+ String(t.day())+"    "+ String(t.hour()) +":"+ String(t.minute()) +":"+ String(t.second()));
+
+  //lidar
+  tfmini.begin(&mySerial);    
+
+  // carte sd et liste des fichiers sur la carte
+  initSDCard();  //carte formatée en FAT32
+  listDir(SD, "/", 0);  
+  initVarHtml();  // preparation  page web
+
+  // wifi
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP(); // generalement 192.168.4.1
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  // serveur
+  //server.begin();
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     // index.html est dans /var/www/html/index.html
     // il contiendra toute le page css et html
-    // avec avant mise à jour de la liste des fichiers obtenus avec dirlist
+    // avec mise à jour de la liste des fichiers obtenus avec dirlist
+    //http://192.168.4.1/index.html
     writeFile(SD, "/index.html", stHTML.c_str());
     request->send(SD, "/index.html", "/text/)($html");
   });
-
-  server.serveStatic("/", SD, "/");
-
+  server.serveStatic("/", SD, "/"); 
   server.begin();
-
   
-}
+  digitalWrite(ledpower, HIGH);
+  delay(500);
+  digitalWrite(ledpassage, LOW);
+  delay(200);
+personne = 0;
+
+};
+
+///////////////////////////////////////////////////////////
 
 void loop() {
-  
+  while (true) {
+/*    //uint16_t dist = tfmini.getDistance();
+    uint16_t dist = 0;
+    
+    if (dist < mini)
+      mini = dist;
+    if ((dist - mini >  seuil) && ((dist + seuil < maxi) || (dist0 + seuil < maxi)) && (nb > precedent + ecart))
+    {  // on etait sur un mini
+      // incremente le nombre de passage
+      digitalWrite(ledpassage, HIGH);
+      EcritFichier(); 
+      personne++;
+      //Serial.print("Nb Personnes :", personne, "  ecart distance :", dist - mini)
+      mini= dist + seuil / 2;  //stoke le nouveau mini
+      dist0 = dist; //memorise la distance
+      precedent = nb; // memorise la derniere mesure significative
+    };
+    digitalWrite(ledpassage, LOW);
+*/
+    delay(1000);
+/*
+    personne++;
+    Serial.print(" personne=");
+    Serial.print(personne);
+    if (DEBUG) EcritFichier();
+    Serial.print(" dist=");
+    Serial.println(dist);
+*/
+  }
 }
+
